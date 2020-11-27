f0904f6293c3c155c48fc14cd6ae9712ba6e126c
Taoyuqing <690230951@qq.com.com>
2020-11-27 15:34:42 +0800

    -m常见的菜单

---
+++ vue-menu-style01/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Main from '../views/Main.vue'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Main',
+    component: Main,
+    redirect:"/home",
+    children: [
+      {
+        path: '/home',
+        name: 'Home',
+        component: () => import('../views/Home.vue')
+      },
+      {
+        path: '/about',
+        name: 'About',
+        component: () => import('../views/About.vue')
+      }
+    ]
+  },
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ vue-menu-style01/src/views/Main.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="left" id="nav">
+      <div><router-link to="/home">Home</router-link></div>
+      <div>
+        <router-link to="/about">About</router-link>
+      </div>
+    </div>
+    <div class="right">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  overflow: hidden;
+}
+.left {
+  height: 100vh;
+  background-color: #ededed;
+  width: 200px;
+  float: left;
+}
+.right {
+  width: calc(100vw - 200px);
+  background-color: #999999;
+  height: 100vh;
+  float: left;
+}
+#nav {
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
